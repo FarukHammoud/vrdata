e713b8df45a586848eb3ce6cbb02e4166103b47c
Faruk Hammoud <farukhammoud@gmail.com>
2020-08-23 18:07:12 +0200

    0007

--- site/auth.py
+++ site/auth.py
@@ -43,7 +43,7 @@ class Auth:
         selected = self.vrdata['users'].find_one({'user':user})
 
         payload = {
-            'user_id': selected['_id'].valueOf(),
+            'user_id': str(selected['_id']),
             'exp': datetime.utcnow() + timedelta(seconds=JWT_EXP_DELTA_SECONDS)
         }
         jwt_token = jwt.encode(payload, JWT_SECRET, JWT_ALGORITHM)
